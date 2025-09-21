08525b8917ec94058523486d56494b32be7a3f24
Netchanok Yindee <tungpang.yindee2548@gmail.com>
2025-09-21 22:19:37 +0700

    fix css

--- classroom_booking/classroom_booking/settings.py
+++ classroom_booking/classroom_booking/settings.py
@@ -133,13 +133,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # If you have a /static directory in your project
-]
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
